9aa866a15614c86ce4b83fe5e26fea82deb32d53
Josh Nothum <joshnothum@gmail.com>
2018-02-01 17:26:39 -0600

    added operators and comment codes

---
+++ script.rb
@@ -0,0 +1,19 @@
+my_num = 3;
+
+puts my_num**4
+
+#everything is an object in Ruby. Hooray! So methods apply to strings
+
+puts "josh".reverse
+
+name = "josh"
+
+puts name.upcase.reverse
+
+=begin
+
+Multi line comments are setup with begin and end. How smart!
+
+=end
+
+
